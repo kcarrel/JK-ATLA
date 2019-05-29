e9a62735b38916af68012f6fcc2ea2904f0acca3
Katie Carrel <katiecarrel@comcast.net>
2019-05-29 15:21:00 -0700

    First commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,23 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { Component} from 'react';
+import atla from './atla.png';
 import './App.css';
 
-function App() {
+class App extends Component {
+
+
+  render () {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+        <img src={atla} alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Guess Who? - Yip Yip edition
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        
       </header>
     </div>
   );
+  }
 }
 
 export default App;
