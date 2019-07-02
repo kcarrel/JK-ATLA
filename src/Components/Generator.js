9081ed3c95ed938d98a1e1abb3aa4e2216b22d4f
Katie Carrel <katiecarrel@comcast.net>
2019-07-01 18:09:52 -0700

    category generator

--- src/Components/Generator.js
+++ src/Components/Generator.js
@@ -1,53 +1,105 @@
 import React, { Component} from 'react';
-import Card from '@material-ui/core/Card';
-import CardActionArea from '@material-ui/core/CardActionArea';
-import CardActions from '@material-ui/core/CardActions';
-import CardContent from '@material-ui/core/CardContent';
-import CardMedia from '@material-ui/core/CardMedia';
 import Button from '@material-ui/core/Button';
 import Typography from '@material-ui/core/Typography';
 import Paper from '@material-ui/core/Paper';
 import Avatar from '@material-ui/core/Avatar';
+import CardContent from '@material-ui/core/CardContent';
+import Card from '@material-ui/core/Card';
+
+import atla from '../atla.png';
 
 let bending = ['airbending', 'waterbending', 'firebending', 'earthbending', 'bloodbending']
 let bended = ['airbended', 'waterbended', 'firebended', 'earthbended', 'bloodbended']
 let woke = ['WOC', 'POC', 'gay', 'queer']
-let general = [`My family comprises of 2 $[bending[Math.rand(0...5)]`]
-let categories = ['birth', 'love', 'woke', 'general']
+let item = ['poop', '']
+let celebrity = ['']
+let categories = ['fetchBirth', 'fetchLovers', 'fetchWoke', 'fetchGeneral']
+let messages = []
+let sign = ['Sagittaruis', 'Pisces', 'Capricorn', 'Aquarius', 'Scorpio', 'Libra', 'Cancer', 'Aries', 'Virgo', 'Leo', 'Gemini', 'Taurus']
 class Generator extends Component {
   constructor() {
     super()
     this.state = {
       generated: false,
-      message: ''
+      message: '',
+      category: ''
     }
   }
 
   handleSubmit = (ev) => {
     ev.preventDefault()
-    this.setState({generated: true})
-    this.fetchCharacter()
+    let rand = [Math.floor(Math.random() * 4)]
+    if (rand == 0) {
+      this.fetchBirth()
+    } else if (rand == 1) {
+      this.fetchLovers()
+    } else if (rand == 2) {
+      this.fetchWoke()
+    } else {
+      this.fetchGeneral()
+    }
   }
 
-  fetchCharacter = (ev) => {
+  handleCategory(){
+    this.setState({
+      category: `${categories[Math.floor(Math.random() * 5)]}`
+    })
+  }
+
+  fetchLovers = (ev) => {
     fetch('https://last-airbender-api.herokuapp.com/api/v1/characters/random?count=2')
     .then(resp => resp.json())
     .then(char => {
       this.setState ({
-        message: `${char[0].name} loved ${char[1].name}`
+        message: `${char[0].name} loved ${char[1].name} most ardently but unfortunately our series could not explore their backstory.`,
+        generated: true
       })
     })
+    console.log("I'm lover")
+  }
+   fetchWoke = (ev) => {
+     console.log("i'm woke")
+   }
+
+  fetchBirth = (ev) => {
+    fetch('https://last-airbender-api.herokuapp.com/api/v1/characters/random')
+    .then(resp => resp.json())
+    .then(char => {
+      this.setState ({
+        message: `${char[0].name} was born in ${[Math.floor(Math.random() * 101)]} AG and is SUCH a ${sign[Math.floor(Math.random() * 13)]}.`
+      })
+    })
+    console.log("i'm birth")
+  }
+
+  fetchGeneral() {
+    console.log("im general")
   }
 
   render() {
     return (
       <main>
         { this.state.generated ? (
-        <Paper className="tweet">
-          <Typography>
-            {this.state.message}
+        <Card className="tweet">
+          <CardContent className="heading">
+
+          <Avatar className="avatar" src={atla}>
+          </Avatar>
+          <Typography className="name" style={{marginLeft: 3, fontSize: 16}}>
+              Alternate ATLA
           </Typography>
-        </Paper> ) : (
+          <Typography className="username" style={{marginLeft: 3, fontSize: 14}}>
+              @JK_ATLA
+          </Typography>
+          </CardContent>
+
+          <CardContent className="container">
+            <Typography className="content">
+              {this.state.message}
+            </Typography>
+          </CardContent>
+
+      </Card> ) : (
           <Paper className="generator">
             <Typography>
               Want an alternate ATLA plot point?
